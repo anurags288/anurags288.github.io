9397d29402d717d208c9f9dc1e63c705c02bb19e
Admin <anurags288@gmail.com>
2020-09-13 17:07:08 +0530

    Updates

---
+++ precache-manifest.13173998b3d66170a76f68289e5ef497.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa0102a0889e9364c019083c59a3ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ab32db1cadd3814aa00e",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab32db1cadd3814aa00e",
+    "url": "/static/js/main.79d796d7.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "8879e005f0570d397b47fce4cac38e11",
+    "url": "/static/media/resume.8879e005.pdf"
+  }
+]);
